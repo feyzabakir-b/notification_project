76f2d4e9fa175207ce5375669ecd0fc7ac541731
feyzabakir-b <feyzabakir51@gmail.com>
2024-08-08 09:01:09 +0300

    index page edited. page rendered

--- notificationapp/views.py
+++ notificationapp/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Notification
 from .serializers import NotificationSerializer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all().order_by('-created_at')
@@ -11,6 +13,21 @@ class NotificationViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        notification = serializer.save()
+        self.send_notification_via_websocket(notification)
+
+    def send_notification_via_websocket(self, notification):
+        channel_layer = get_channel_layer()
+        group_name = f"user_{notification.user.id}"
+        async_to_sync(channel_layer.group_send)(
+            group_name,
+            {
+                'type': 'send_notification',
+                'message': notification.message,
+            }
+        )
 
 def index(request):
     return render(request, 'notifications/index.html')
